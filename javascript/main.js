b2d35360a3f515af4967a6ced82f576147683091
Amarjeet Singh <singhamarjeet0045@gmail.com>
2019-07-13 22:02:25 +0530

    after adding js setup

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+const textForm = document.querySelector('form');
+const textInput = document.querySelector('#text-input');
+const voiceSelect = document.querySelector('#voice-select');
+const rate = document.querySelector('#rate');
+const rateValue = document.querySelector('#rate-value');
+const pitch = document.querySelector('#pitch');
+const pitchValue = document.querySelector('#pitch-value');
+

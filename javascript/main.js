8c9be30bbe175eee3597fdd3447a27dfc1be8523
Amarjeet Singh <singhamarjeet0045@gmail.com>
2019-07-14 10:31:38 +0530

    before final styling

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,77 @@
-const textForm = document.querySelector('form');
-const textInput = document.querySelector('#text-input');
-const voiceSelect = document.querySelector('#voice-select');
-const rate = document.querySelector('#rate');
-const rateValue = document.querySelector('#rate-value');
-const pitch = document.querySelector('#pitch');
-const pitchValue = document.querySelector('#pitch-value');
+const synth = window.speechSynthesis;
+const textForm = document.querySelector("form");
+const textInput = document.querySelector("#text-input");
+const voiceSelect = document.querySelector("#voice-select");
+const rate = document.querySelector("#rate");
+const rateValue = document.querySelector("#rate-value");
+const pitch = document.querySelector("#pitch");
+const pitchValue = document.querySelector("#pitch-value");
 
+let voices = [];
+const getVoices = () => {
+  voices = synth.getVoices();
+
+  //this is the logic for getting the language in the select bar
+  voices.forEach(voice => {
+    const option = document.createElement("option");
+    option.textContent = voice.name + "(" + voice.lang + ")";
+
+    option.setAttribute("data-lang", voice.lang);
+    option.setAttribute("data-name", voice.name);
+    voiceSelect.appendChild(option);
+  });
+};
+getVoices();
+
+if (synth.onvoiceschanged !== undefined) {
+  synth.onvoiceschanged = getVoices;
+}
+
+const speak = () => {
+  if (synth.speaking) {
+    console.error("Already Speaking ..");
+    return;
+  }
+  if (textInput.value !== "") {
+    const speakText = new SpeechSynthesisUtterance(textInput.value);
+
+    speakText.onend = e => {
+      console.log("Done Speaking ...");
+    };
+
+    speakText.onerror = e => {
+      console.error("Something Went Wrong ...");
+    };
+    const selectedVoice = voiceSelect.selectedOptions[0].getAttribute(
+      "data-name"
+    );
+
+    voices.forEach(voice => {
+      if (voice.name === selectedVoice) {
+        speakText.voice = voice;
+      }
+    });
+    speakText.rate = rate.value;
+    speakText.pitch = pitch.value;
+    synth.speak(speakText);
+  }
+};
+
+
+//Event Listeners
+
+//Text Form Submit
+
+textForm.addEventListener('submit', e => {
+    e.preventDefault();
+    speak()
+    textInput.blur();
+})
+
+rate.addEventListener('change', e => (rateValue.textContent = rate.value));
+
+// Pitch value change
+pitch.addEventListener('change', e => (pitchValue.textContent = pitch.value));
+
+// Voice select change
+voiceSelect.addEventListener('change', e => speak());
